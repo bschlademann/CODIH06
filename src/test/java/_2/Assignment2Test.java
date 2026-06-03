6fb7cedc7fb841bfc361421ca12de9e9ac8fbb79
Benni-beep <krikelkr4kel@gmail.com>
2026-06-03 14:00:28 +0200

    feat: add C2 and boundary tests for assignment 2

--- src/test/java/_2/Assignment2Test.java
+++ src/test/java/_2/Assignment2Test.java
@@ -1,25 +1,24 @@
 package _2;
 
 import junitparams.JUnitParamsRunner;
-import org.junit.After;
-import org.junit.Before;
+import junitparams.Parameters;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static org.junit.Assert.assertEquals;
 
 @RunWith(JUnitParamsRunner.class)
 public class Assignment2Test {
-    private Assignment2 assignment2;
 
-    @Before
-    public void before() {
-        assignment2 = new Assignment2();
+    @Test
+    @Parameters({
+            "-1     ,               ",  // negative input (boundary value)
+            "1      ,               ",  // lower boundary, no prime factors
+            "2      ,   2           ",  // while-loop (n%2==0), smallest prime
+            "3      ,   3           ",  // if-path (n>2), smallest odd prime
+            "9      ,   3 3         ",  // for+inner while (i≤√n, n%i==0), odd square
+    })
+    public void primeFactors(int input, String expected) {
+        assertEquals(expected, Assignment2.primeFactors(input));
     }
-
-    @After
-    public void after() {
-        assignment2 = null;
-    }
-
-
 }
