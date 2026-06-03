1f609896eecb122a33c0ec591b7ad451f29e7d1e
Benni-beep <krikelkr4kel@gmail.com>
2026-06-03 14:39:45 +0200

    initial setup for assignment 3

---
+++ src/main/java/_3/Assignment3.java
@@ -0,0 +1,18 @@
+package _3;
+
+public class Assignment3 {
+    private final int[][] matrix = {
+            {0, 0, 0, 0, 0, 0},
+            {0, 1, 0, 0, 0, 0},
+            {0, 0, 1, 0, 0, 0},
+            {0, 0, 0, 1, 0, 0},
+            {0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0},
+    };
+
+    private record LineStart(int row, int col){};
+
+    static void main() {
+    }
+
+}
