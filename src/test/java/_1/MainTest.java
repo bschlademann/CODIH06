fae3a8be07b97196a1a58084320c41086f6e232e
Benni-beep <krikelkr4kel@gmail.com>
2026-06-03 10:07:05 +0200

    feat: add tests for assignment 1a

---
+++ src/test/java/_1/MainTest.java
@@ -0,0 +1,42 @@
+package _1;
+
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(JUnitParamsRunner.class)
+public class MainTest {
+    private Main main;
+
+    @Before
+    public void before() {
+        main = new Main();
+    }
+
+    @After
+    public void after() {
+        main = null;
+    }
+
+    @Test
+    public void testC0() {
+        main.a(1);
+        assertEquals(1, main.getX());
+    }
+
+    @Test
+    @Parameters({"1", "0"})
+    public void testC1(int x) {
+        main.a(x);
+        if (x > 0) {
+            assertEquals(x, main.getX());
+        } else {
+            assertEquals(0, main.getX());
+        }
+    }
+}
